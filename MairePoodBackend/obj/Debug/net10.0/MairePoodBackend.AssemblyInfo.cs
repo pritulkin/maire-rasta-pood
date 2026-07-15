1cbb70049009a4ffbea380fe755d3eb091bcfae2
pritulkin <priit.linnakivi@gmail.com>
2026-07-15 13:07:52 +0300

    repair

--- MairePoodBackend/obj/Debug/net10.0/MairePoodBackend.AssemblyInfo.cs
+++ MairePoodBackend/obj/Debug/net10.0/MairePoodBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MairePoodBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685e918b365dfcebac422f0c15114d5bf07b2fc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc28d3d93b27bda2e1eddf6a013e823217e3789")]
 [assembly: System.Reflection.AssemblyProductAttribute("MairePoodBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MairePoodBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
